6db4e59fb3854024506377f16a6c84bdb7b6501f
Joab-David-Johanan <joabdavid96@gmail.com>
2025-06-15 20:04:52 +0200

    added new question and solution - smallest positive integer

---
+++ 2. Smallest_integer/smallest_interger.py
@@ -0,0 +1,70 @@
+class Solution(object):
+    def smallest_positive_number(self, A):
+        n = len(A)
+        # cleaning the array
+        for i in range(n):
+            if A[i] <= 0 or A[i] > n:
+                A[i] = n + 1
+
+        # marking the presence of elements from [1...n] with -ve sign
+        for i in range(n):
+            val = abs(A[i])
+            if 1 <= val <= n:
+                A[val - 1] = -abs(A[val - 1])
+
+        # returning the first positive number we find
+
+        for i in range(n):
+            if A[i] > 0:
+                return i + 1
+
+        # if we have all elements from [1...n] as -ve values then the only solution is n+1 so we return it
+        return n + 1
+
+    def smallest_non_negative_number(self, A):
+        n = len(A)
+
+        # Replace negative numbers and numbers greater than n with n+1
+        for i in range(n):
+            if A[i] < 0 or A[i] > n:
+                A[i] = n + 1
+
+        # Mark the presence of values in [0..n]
+        for i in range(n):
+            val = abs(A[i])
+            if 1 <= val <= n:
+                A[val - 1] = -abs(A[val - 1])
+
+        # Check if 0 is missing before we check for positive numbers
+        if 0 not in A:
+            return 0
+
+        # Now look for first positive index -> missing number
+        for i in range(n):
+            if A[i] > 0:
+                return i + 1
+
+        # All numbers from 0..n are present
+        return n + 1
+
+    def set_implementation(self, A):
+        s = set(A)
+        i = 1
+        while i in s:
+            i += 1
+        return i
+
+    def set_another_version(self, A):
+        s = set(A)
+        i = 0
+        while i in s:
+            i += 1
+        return i
+
+
+obj = Solution()
+result = obj.smallest_positive_number([0, -1, 10, 2, 3, 4])
+result1 = obj.smallest_non_negative_number([-1, 1, 2, 3, 4, 5, -10])
+result2 = obj.set_implementation([0, -1, 10, 2, 3, 4])
+result3 = obj.set_another_version([-1, 1, 2, 3, 4, 5])
+print(result, result1, result2, result3, sep='\n')
